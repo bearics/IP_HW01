18d46430ea4bf3cc3928fad714ef602196c7f019
bearics <bearics@gmail.com>
2018-10-07 04:32:27 +0900

    fin hw01

--- IP_HW01/main.cpp
+++ IP_HW01/main.cpp
@@ -22,10 +22,6 @@ unsigned char** memAllocate2D(int sx, int sy)
 	return data;
 }
 
-
-
-
-
 unsigned char** readFile(string path, int sx, int sy)
 {
 	// read file function
@@ -68,41 +64,10 @@ int writeFile(string path, unsigned char** data, int sx, int sy)
 }
 
 
-void rotateRAW(unsigned char** data, int sx, int sy, double degree)
-{
-	unsigned char** outData = memAllocate2D(sx, sy);
 
-	int ori_x, ori_y;
-	unsigned char pixel;
-	double rad = degree * PI / 180.0;
-	double cc = cos(rad), ss = sin(-rad);
-	double xc = (double)sx / 2.0, yc = (double)sy / 2.0;	// x,y 's center
-
-	for (int y = 0; y < sy; y++)
-	{
-		for (int x = 0; x < sx; x++)
-		{
-			ori_x = (int)(xc + ((double)y - yc) * ss + ((double)x - xc) * cc);
-			ori_y = (int)(yc + ((double)y - yc) * cc - ((double)x - xc) * ss);
-			pixel = (unsigned char)0;
-			if (( (ori_y >= 0 && ori_y < sy) && (ori_x >= 0 && ori_x < sx) ))
-				pixel = data[ori_y][ori_x];
-			outData[y][x] = pixel;
-		}
-	}
-
-	string path = "c:\\lena256_rotate_" + to_string((int)(degree)) + ".raw";
-
-	writeFile(path, outData, (int)sx, (int)sy);
-	cout << "finish to write file\n";
-
-}
-
-unsigned char bilinearInterpolation(unsigned char** data, int x, int y, int sx, int sy, double scale)
+unsigned char bilinearInterpolation(unsigned char** data, double ori_x, double ori_y, int sx, int sy)
 {
 	unsigned char m[2][2];
-	double ori_x = x / scale;
-	double ori_y = y / scale;
 
 	for (int i = 0; i < 2; i++)
 	{
@@ -122,6 +87,37 @@ unsigned char bilinearInterpolation(unsigned char** data, int x, int y, int sx, 
 	return (unsigned char)(d4 * (d2 * (int)m[0][0] + d1 * (int)m[0][1]) + d3 * (d2 * (int)m[1][0] + d1 * (int)m[1][1]));
 }
 
+
+void rotateRAW(unsigned char** data, int sx, int sy, double degree)
+{
+	unsigned char** outData = memAllocate2D(sx, sy);
+
+	double ori_x, ori_y;
+	unsigned char pixel;
+	double rad = -degree * PI / 180.0;
+	double cc = cos(rad), ss = sin(-rad);
+	double xc = (double)sx / 2.0, yc = (double)sy / 2.0;	// x,y 's center
+
+	for (int y = 0; y < sy; y++)
+	{
+		for (int x = 0; x < sx; x++)
+		{
+			ori_x = xc + ((double)y - yc) * ss + ((double)x - xc) * cc;
+			ori_y = yc + ((double)y - yc) * cc - ((double)x - xc) * ss;
+			pixel = (unsigned char)0;
+			if (((ori_y >= 0 && ori_y < sy) && (ori_x >= 0 && ori_x < sx)))
+				pixel = bilinearInterpolation(data, ori_x, ori_y, 256, 256);
+			outData[y][x] = pixel;
+		}
+	}
+
+	string path = "c:\\lena256_rotate_" + to_string((int)(degree)) + ".raw";
+
+	writeFile(path, outData, (int)sx, (int)sy);
+	cout << "finish to write file\n";
+
+}
+
 void scaleRAW(unsigned char** data, int sx, int sy, double scale)
 {
 	int new_sx = (int)sx*scale;
@@ -133,7 +129,7 @@ void scaleRAW(unsigned char** data, int sx, int sy, double scale)
 	{
 		for (int x = 0; x < new_sx; x++)
 		{
-			outData[y][x] = bilinearInterpolation(data, x, y, sx, sy, scale);
+			outData[y][x] = bilinearInterpolation(data, (double)x / scale , (double)y / scale, sx, sy);
 		}
 	}
 
@@ -150,8 +146,11 @@ int main(void)
 
 	data = readFile("c:\\lena256.raw", 256, 256);
 	
-	//scaleRAW(data, 256, 256, 436.0 / 256.0);
-	rotateRAW(data, 256, 256, 30.0);
+	scaleRAW(data, 256, 256, 436.0 / 256.0);	// scale image
+	scaleRAW(data, 256, 256, 512.0 / 256.0);	// scale image
+	rotateRAW(data, 256, 256, 30.0);	// rotate image
+	rotateRAW(data, 256, 256, 45.0);	// rotate image
+	rotateRAW(data, 256, 256, 60.0);	// rotate image
 
 	return 0;
 }
